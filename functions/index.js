9161e5d17397a456840caec6784edd2337ac573e
Josh Mogil <jmogil@pop-os.localdomain>
2023-01-02 12:28:03 -0500

    getting response back in browser

--- functions/index.js
+++ functions/index.js
@@ -1,37 +1,32 @@
 const functions = require("firebase-functions");
-const { Configuration, OpenAIApi } = require("openai");
+const {Configuration, OpenAIApi} = require("openai");
 const cors = require("cors")({origin: true});
 // Add CORS to your index.js
-
-
-// Automatically allow cross-origin requests
-
 
 exports.getTranslation = functions.https.onRequest((request, response) => {
   cors(request, response, () => {});
   const data = request.body;
-
+  console.log(data);
   const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
   });
+
   const openai = new OpenAIApi(configuration);
-  
-  const response = await openai.createCompletion({
+  // console.log(data);
+  // console.log(openai);
+
+  openai.createCompletion({
     model: "text-davinci-003",
-    prompt: "",
+    prompt: "Hello there",
     temperature: 0.7,
     max_tokens: 256,
     top_p: 1,
     frequency_penalty: 0,
     presence_penalty: 0,
+  }).then((apiResponse) => {
+    const apiData = apiResponse.data.choices[0].text;
+    console.log(`OPENAI result: ${apiData}`);
+    response.status(200).send({data: apiData});
   });
-
-
-  functions.logger.info("Hello logs!", {structuredData: true});
-  functions.logger.info(data, {structuredData: true});
-  response.status(200).send({data: "json"});
 });
-
-
-
 
